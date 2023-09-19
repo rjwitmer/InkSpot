e628e786265d56a86b1d0f21a42195eb74f7e174
Bob Witmer <bwitmer@golden.net>
2023-09-19 15:43:17 -0400

    Initial Commit

---
+++ InkSpot/InkSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkSpotApp.swift
+//  InkSpot
+//
+//  Created by Bob Witmer on 2023-09-19.
+//
+
+import SwiftUI
+
+@main
+struct InkSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
