0e5e1bb5e4af6eb38232fe81eb21f83be37607ca
Bob Witmer <bwitmer@golden.net>
2023-09-23 16:54:37 -0400

    Finished task 11 on the exam

---
+++ InkSpot/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  InkSpot
+//
+//  Created by Bob Witmer on 2023-09-21.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
